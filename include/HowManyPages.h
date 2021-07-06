626ce536ed8b91af6dd73c79bc8769ea50239250
jarek69 <roszyk.jarek@gmail.com>
2021-07-06 18:54:11 +0200

    Rename headers file

---
+++ include/HowManyPages.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+
+
+class HowManyPages
+{
+public:
+    std::string DateOfReading;
+    int NumberOfPagesRead = 0;
+    int SumPages = 0;
+    void enterdata();
+    void sum(); 
+    std::string  enterTitle() const;
+    static void SaveToFile(HowManyPages &savedate, std::string &tit);
+    
+};
+
+void menu();
+int calc();
+
