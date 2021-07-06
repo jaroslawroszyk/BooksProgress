266e769e02261c323acd477f03b160b8f3d5ddb4
jarek69 <roszyk.jarek@gmail.com>
2021-07-06 21:25:46 +0200

    Adding const function

--- include/HowManyPages.h
+++ include/HowManyPages.h
@@ -8,9 +8,9 @@ public:
     int NumberOfPagesRead = 0;
     int SumPages = 0;
     void enterdata();
-    void sum(); 
+    void sum() const; 
     std::string  enterTitle() const;
-    static void SaveToFile(const HowManyPages &savedate, std::string &tit);
+    static void SaveToFile(const HowManyPages &savedate, std::string &BookTitle);
 };
 
 void menu();
