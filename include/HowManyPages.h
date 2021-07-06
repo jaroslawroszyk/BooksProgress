e3d42eb5fc4272f8d09eafabaf8c09e6716c44ce
jarek69 <roszyk.jarek@gmail.com>
2021-07-06 22:50:54 +0200

    Renaming enum class correlates with the changes in the .cpp file so I…
    … changed the old name to the new one, and add const function

--- include/HowManyPages.h
+++ include/HowManyPages.h
@@ -8,11 +8,11 @@ public:
     int NumberOfPagesRead = 0;
     int SumPages = 0;
     void enterdata();
-    void sum() const; 
-    std::string  enterTitle() const;
-    static void SaveToFile(const HowManyPages &savedate, std::string &BookTitle);
+    void sum() const;
+    std::string enterTitle() const;
+    void SaveToFile(const std::string &BookTitle) const;
+    // void SaveToFile(const HowManyPages &savedate, std::string &BookTitle) const;
 };
 
 void menu();
 int calc();
-
