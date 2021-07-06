24496d8f4408a9d8951c1fa309242b5d7c43e33e
jarek69 <roszyk.jarek@gmail.com>
2021-07-06 18:55:31 +0200

    .

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,10 @@
-#include "pages.h"
+#include "HowManyPages.h"
+#include "Pages.h"
 #include <iostream>
-
-
 
 int main()
 {
-    HowManyPages m;
-    std::cout << "APPLICATION TO SAVE HOW MANY PAGES YOU READ PER DAY\n";
-    m.menu();
+
+    // std::cout << "APPLICATION TO SAVE HOW MANY PAGES YOU READ PER DAY\n";
+    menu();
 }
