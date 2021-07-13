503dbbb9089f576d7fb194377737bb2541979d79
jarek69 <roszyk.jarek@gmail.com>
2021-07-13 16:48:21 +0200

    adding a component responsible for showing the content of a given file

--- include/ProgramMode.h
+++ include/ProgramMode.h
@@ -6,10 +6,10 @@ enum class ProgramMode
     p_PagesDay = 1,
     p_Sum = 2 ,
     p_Show = 3,
-    p_delete = 4,
-    p_Calculate = 5,
-    p_exit = 6,
+    p_ShowTheConent = 4,
+    p_delete = 5,
+    p_Calculate = 6,
+    p_exit = 7,
 };
  
-
 std::istream &operator>>(std::istream &in, ProgramMode &enter);
